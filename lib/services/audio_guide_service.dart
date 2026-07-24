284e5f608e4757b3b15d0a5c2fca4ceeed82b320
Thomas Arnaud <38663853+Nohzoh@users.noreply.github.com>
2026-07-25 00:51:07 +0100

    feat: integrate AppLogger in lib/services/audio_guide_service.dart

--- lib/services/audio_guide_service.dart
+++ lib/services/audio_guide_service.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import '../utils/app_logger.dart';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter/foundation.dart';
